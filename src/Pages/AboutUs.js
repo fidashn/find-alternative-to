a5272b31b2df4ff15116f8df0ade420b8f13eebb
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-30 18:38:46 +0300

    added meaningfull texts to about us & home pages

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -4,7 +4,15 @@ function AboutUs() {
   return (
     <div className='aboutUs-container'>
     <h2 className='aboutUs-title'>About Us</h2>
-    <p className='aboutUs-p'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat ipsam nulla ad voluptatibus dicta, dolores impedit quos eaque numquam totam odit rem, iusto facilis a porro doloremque ullam blanditiis quia iure. Aspernatur, aliquam! Quisquam, hic beatae eaque doloribus voluptatum quaerat, voluptatibus explicabo pariatur quibusdam unde, maxime optio. Minima inventore voluptate accusantium ipsa libero odio, consequuntur quis id delectus et quas ullam sunt excepturi aut repellat soluta magni rerum facere debitis eius illum distinctio provident sed! Porro aliquam rerum asperiores architecto! Facere, laudantium illo dolor quasi nesciunt, minima eveniet veniam minus omnis soluta debitis rem suscipit sed nemo. Quidem, error tempora.</p>
+    <p className='aboutUs-p'>we are a retail company that provides customers to compare products prices, features, or hot deals!. 
+    we work with the biggest cooperations in the world such as:
+    <li>Amazon.</li>
+    <li>Ebay</li>
+    <li>Ali baba</li>
+     with the help of our amazing professional staff you will never miss on any hot deal!.
+     <br />
+     *especially on holidays and big sales compains.
+    </p>
     </div>
   )
 }
