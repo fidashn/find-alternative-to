a994267d5e28cedd1109016afe648a0bd3455118
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-31 00:27:54 +0300

    did final changes in responsiveness

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -5,25 +5,25 @@ function Products() {
   return (
      <div className='product-page'>
     <div className='product-container'>
-      <div className="product-img"></div>
-      <div className="product-short-info">
+      <div className="product-img "></div>
+      <div className="product-short-info ">
         <p className="product-short-info-p">
           Sony 85 Inch Bravia Xr X95J Full Array Led Smart Google Tv,
           4K Ultra Hd High Dynamic Range Hdr, Xr-85X95J, 2021 Model.</p>
-        <button className='discount'>50% off</button>
+        <button className='discount '>50% off</button>
       </div>
-      <div className="rating">
+      <div className="rating ">
         <div className="rating-numeric">9.8</div>
         <div className="rating-stars"></div>
       </div>
-      <div className="more-info">
+      <div className="more-info ">
       <button className='view-deal-btn'>View Deal</button>
       <div className="show-more">
-      <button className='on-amazon-btn'>On Amazon</button>
-      <button className='show-more-btn'>Show More <RiArrowDownSLine /></button>
+      <button className='on-amazon-btn '>On Amazon</button>
+      <button className='show-more-btn '>Show More <RiArrowDownSLine /></button>
       </div>
       </div>
-    </div>
+      </div>
     <div className='product-container'>
       <div className="product-img2"></div>
       <div className="product-short-info">
