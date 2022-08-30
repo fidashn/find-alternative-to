e628a1f242705e99afe1c17bfbf51b3c1b4cfde4
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-30 14:47:04 +0300

    removed unnecessary files & changed icon

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      Find Alternative To &copy; 2022
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'> 
+    <ul className='navbar-items'>
+    <li>Home</li>
+    <li>About Us</li>
+    <li>Our Services</li>
+    <li>Contact Us</li>
+    </ul>
+    {/* <div className='navbar-btns'>
+    <button className='sign-inup btn'>Sign Up</button>
+    <button className='sign-in btn'>Sign In</button>
+    </div> */}
+    </div>
+  )
+}
+
+export default Navbar
